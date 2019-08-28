87419c7b63eb9bb858f5deea5d2eb599c77b88a7
Vandesm14 <vandesm14@gmail.com>
2019-08-28 14:42:04 -0400

    Revert "added db.json to gitignore"
    This reverts commit 4011634eea1868f1d463bdbed489f47dce9a2d9a.

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ app.get('/:room/', (req, res) => {
 app.get('/:room/:user', (req, res) => {
 	let room = req.params.room;
 	let user = req.params.user;
-	if (/\W/.test(user)) { // Illegal Username
+	if (/\W/.test(user)) {
 		res.send('[LowChat] Error: Illegal characters present in username. The legal characters are "A-Z", "a-z", and "_".');
 		// console.log('Illegal Username');
-	} else if (/\W/.test(room)) { // Illegal Username
+	} else if (/\W/.test(room)) {
 		res.send('[LowChat] Error: Illegal characters present in room name. The legal characters are "A-Z", "a-z", and "_".');
 		// console.log('Illegal Room Name');
 	} else if (db[user] === undefined) { // User does not exist in entire DB
