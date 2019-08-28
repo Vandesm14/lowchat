14d59d4d7f865ca40b334d1265f5912b56da5c2c
Vandesm14 <vandesm14@hyperjs-servers.com>
2019-08-28 01:18:17 -0400

    beta release

--- app.js
+++ app.js
@@ -2,10 +2,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const camelCase = require('camelcase');
 
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+io.engine.generateId = function (req) {
+	return randID();
+};
 
 var db = {};
 
@@ -15,12 +20,16 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static('docs'));
 
-http.listen(3000, function () {
-	console.log('listening on *:3000');
+http.listen(5500, function () {
+	console.log('listening on *:5500');
 });
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/docs/index.html');
+});
+
+app.get('/:room/', (req, res) => {
+	res.sendFile(__dirname + '/docs/pages/room.html');
 });
 
 app.get('/:room/:user', (req, res) => {
@@ -28,23 +37,21 @@ app.get('/:room/:user', (req, res) => {
 	let user = req.params.user;
 	if (/\W/.test(user)) {
 		res.send('[LowChat] Error: Illegal characters present in username. The legal characters are "A-Z", "a-z", and "_".');
-		console.log('Illegal Username');
+		// console.log('Illegal Username');
 	} else if (/\W/.test(room)) {
 		res.send('[LowChat] Error: Illegal characters present in room name. The legal characters are "A-Z", "a-z", and "_".');
-		console.log('Illegal Room Name');
+		// console.log('Illegal Room Name');
 	} else if (db[user] === undefined) { // User does not exist in entire DB
 		db[user] = {
 			room: room
 		};
-		res.sendFile(__dirname + '/docs/pages/room.html');
-		console.log('New User');
+		res.sendFile(__dirname + '/docs/pages/app.html');
+		// console.log('New User');
 	} else if (Object.keys(db).find(obj => db[obj].room === room)) { // Duplicate User
 		res.send('[LowChat] Error: The user "' + user + '" already exists in the room. Please try a different username.');
-		console.log('Duplicate User');
+		// console.log('Duplicate User');
 	}
-	fs.writeFile('db.json', JSON.stringify(db), 'utf8', () => {
-		console.log('DB Updated');
-	});
+	fs.writeFile('db.json', JSON.stringify(db), 'utf8', () => {});
 });
 
 io.on('connection', function (socket) {
@@ -52,19 +59,17 @@ io.on('connection', function (socket) {
 		let user = data.user;
 		let room = data.room;
 
-		if (db[user] === undefined) {
+		socket.join(room);
+		socket.emit('init-back', randID());
+		socket.to(room).emit('server', `User ${user} has joined the channel`);
+
+		if (db[user] === undefined) { // User doesn't already exist, add ID
 			db[user] = {
 				room: room,
 				id: socket.id
 			};
 		} else {
-			// db.find((obj) => {
-			// 	if (obj.room === room) {
-			// 		obj.id = socket.id;
-			// 		return;
-			// 	}
-			// });
-			Object.keys(db).find((obj) => {
+			Object.keys(db).find((obj) => { // User exists, add ID
 				if (db[obj].room === room) {
 					db[obj].id = socket.id;
 					return;
@@ -72,24 +77,39 @@ io.on('connection', function (socket) {
 			});
 		}
 		console.log(`User ${socket.id} is now known as "${user}"`);
+		fs.writeFile('db.json', JSON.stringify(db), 'utf8', () => {});
+	});
 
-		fs.writeFile('db.json', JSON.stringify(db), 'utf8', () => {
-			console.log('DB Updated')
+	socket.on('message', function (data) {
+		Object.keys(db).find((obj) => {
+			if (db[obj].id === socket.id && obj === data.user) { // Note SocketID is unique so checking for room isn't needed
+				socket.to(db[obj].room).emit('message', {
+					user: data.user,
+					message: sanitize(data.message)
+				});
+				return;
+			}
 		});
 	});
 
 	socket.on('disconnect', function () {
-		console.log('Client Disconnected');
+		// console.log('Client Disconnected');
 		Object.keys(db).find((obj) => {
 			if (db[obj].id === socket.id) { // Note SocketID is unique so checking for room isn't needed
+				socket.to(db[obj].room).emit('server', `User ${obj} has left the channel`);
 				delete db[obj];
-				console.log('Deleted "' + obj + '" from the DB');
+				// console.log('Deleted "' + obj + '" from the DB');
 				return;
 			}
 		});
-	})
+		fs.writeFile('db.json', JSON.stringify(db), 'utf8', () => {});
+	});
 });
 
 function sanitize(text) {
 	return text.replace(/<(?:.|\n)*>/gi, '[html]');
+}
+
+function randID() {
+	return Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
 }
--- docs/js/script.js
+++ docs/js/script.js
@@ -1,29 +1,72 @@
 var client;
 var user = window.location.href.split('/');
 var room = window.location.href.split('/');
+var id = '';
 user = user[user.length - 1];
 room = room[room.length - 2];
 
 
 $(document).ready(function () {
-	client = io.connect('http://127.0.0.1:3000');
+	client = io.connect('http://v14server.ml:5500');
 
 	client.on('connect', function () {
-		client.emit('init', {user: user, room: room});
+		let template = $('#message').attr('placeholder')
+		template = template.replace('{{user}}', user);
+		template = template.replace('{{room}}', room);
+		$('#message').attr('placeholder', template);
+
+		client.emit('init', {
+			user: user,
+			room: room
+		});
 	});
 
-	client.on('message', function(){});
-	client.on('server', function(){});
+	client.on('init-back', function (data) {
+		id = data;
+	});
+
+	client.on('message', function (data) {
+		let template = $('#lineTemplate').html();
+		template = template.replace('{{user}}', data.user);
+		template = template.replace('{{message}}', data.message);
+		template = template.replace(new RegExp('@' + user), '<b class="mention">$&</b>');
+		$('.history').append(template);
+		$(".history").animate({
+			scrollTop: $('.history')[0].scrollHeight
+		}, 500);
+	});
+
+	client.on('server', function (data) {
+		let template = $('#serverLineTemplate').html();
+		template = template.replace('{{message}}', data);
+		template = template.replace(new RegExp('@' + user), '<b class="mention">$&</b>');
+		$('.history').append(template);
+		$(".history").animate({
+			scrollTop: $('.history')[0].scrollHeight
+		}, 500);
+	});
 
 	$('#message').on('keydown', function (e) {
 		if (e.key === 'Enter') {
 			sendMessage($(this));
 		}
-	})
+	});
 });
 
 function sendMessage(elem) {
 	let message = elem.val();
 	elem.val('');
-	alert(message);
+	client.emit('message', {
+		user: user,
+		message: message,
+		id: id
+	});
+	let template = $('#lineTemplate').html();
+	template = template.replace('{{user}}', user);
+	template = template.replace('{{message}}', message);
+	template = template.replace(new RegExp('@' + user), '<b class="mention">$&</b>');
+	$('.history').append(template);
+	$(".history").animate({
+		scrollTop: $('.history')[0].scrollHeight
+	}, 500);
 }
