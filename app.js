6f53661f79e895f1c708dafd38cc56e2fce1445d
vandesm14 <vandesm14@gmail.com>
2019-11-19 15:06:34 -0500

    purge old files

--- app.js
+++ app.js
@@ -1,151 +1,152 @@
-/* jshint esversion: 6 */
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const camelCase = require('camelcase'); // NOTE: Unused
 
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
-fs.writeFile('db.json', '', 'utf8', () => {});
-io.engine.generateId = function (req) {
-	return randID();
-};
-
-var db = {
-	users: {},
-	rooms: {}
-};
+const io = require('socket.io').listen(http);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-app.use(express.static('docs'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-http.listen(80, function () {
-	console.log('listening on *:80');
-});
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/docs/index.html');
+	res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/:room/', (req, res) => {
-	res.sendFile(__dirname + '/docs/pages/room.html');
-});
-
-app.get('/:room/:user', (req, res) => {
-	let room = req.params.room;
-	let user = req.params.user;
-	let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-	ip = ip.replace('::ffff:', '');
-
-	if (/\W/.test(user)) { // Illegal Username
-		res.send('[LowChat] Error: Illegal characters present in username. The legal characters are "A-Z", "a-z", and "_".');
-	} else if (/\W/.test(room)) { // Illegal Room Name
-		res.send('[LowChat] Error: Illegal characters present in room name. The legal characters are "A-Z", "a-z", and "_".');
-	} else if (Object.keys(db.users).find(obj => db.users[obj].room === room && obj === user && db.users[obj].ip !== ip)) { // Duplicate User
-		res.send('[LowChat] Error: The user "' + user + '" already exists in the room. Please try a different username.');
-	} else if (Object.keys(db.users).find(obj => db.users[obj].ip === ip && obj === user)) { // Duplicate IP as User
-		res.send('[LowChat] Error: The user "' + user + '" is already assigned to an IP. Please try a different username.');
-	} else if (!Object.keys(db.users).find(obj => db.users[obj].room === room && obj === user)) { // New User
-		res.sendFile(__dirname + '/docs/pages/app.html');
-	} else {
-		res.status(501).send('[LowChat] Error: The server has encountered an error upon joining the room. Please return to the <a href="/">homepage</a>.');
-	}
-	fs.writeFile('db.json', JSON.stringify(db, null, "\t"), 'utf8', () => {});
+app.get('/:room', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('*', (req, res) => {
-	// res.sendFile(__dirname + '/docs/pages/error.html');
-	res.status(404).send('Server Error: 404 Not Found');
+	res.send('404 Not Found');
 });
 
-app.get('*', (req, res) => {
-	// res.sendFile(__dirname + '/docs/pages/error.html');
-	res.status(404).send('Server Error: 404 Not Found');
-});
+http.listen(3000, () => console.log('server started'));
 
-io.on('connection', function (socket) {
-	socket.on('init', function (data) {
-		let user = data.user;
-		let room = data.room;
-		let ip = (socket.handshake.headers["x-real-ip"] || socket.conn.remoteAddress).replace('::ffff:', '');
+io.engine.generateId = (req) => {
+	return randHex(6);
+};
 
-		socket.join(room);
-		socket.emit('init-back', randID());
-
-		if (db.users[user] === undefined) { // User doesn't already exist, add ID, add IP
-			db.users[user] = {
-				room: room,
-				id: socket.id,
-				ip: ip
-			};
-		} else { // TODO: Determin wether to keep or delete this (it's never used)
-			Object.keys(db.users).find((obj) => {
-				if (db.users[obj].room === room && obj === user) { // Checks for current user
-					db.users[obj].id = socket.id;
-					db.users[obj].ip = ip;
-					return;
-				}
-			});
-		}
-		socket.to(room).emit('server', `User ${user} has joined the channel`);
-		console.log(`User ${socket.id} is now known as "${user}"`);
-		fs.writeFile('db.json', JSON.stringify(db, null, "\t"), 'utf8', () => {});
-	});
-
-	socket.on('message', function (data) {
-		let ip = (socket.handshake.headers["x-real-ip"] || socket.conn.remoteAddress).replace('::ffff:', '');
+io.on('connection', (socket) => {
+	socket.on('join', (data) => {
 		let room;
-
-		Object.keys(db.users).find((obj) => { // Find room in which user is in
-			if (db.users[obj].id === socket.id && obj === data.user) {
-				room = db.users[obj].room;
-				return;
-			}
-		});
-
-		if (data.message[0] === '/') { // If message is a command
-			switch (data.message.split(' ')[0]) {
-				case '/users':
-					let message = [];
-					Object.keys(db.users).find((obj) => {
-						if (db.users[obj].room === room) {
-							message.push(obj);
-						}
-					});
-					socket.emit('server', message.join(', '));
-					break;
-			}
+		let sockets = io.sockets.clients().sockets;
+		defaults(socket);
+		if (data && data !== '/') {
+			room = data.substr(1).replace(/\W/g, '');
 		} else {
-			socket.to(room).emit('message', {
-				user: data.user,
-				message: sanitize(data.message)
-			});
+			room = 'main';
+		}
+		socket.join(room);
+		socket.proto.room = room;
+		socket.proto.name = socket.id;
+
+		socket.to(room).emit('message', { name: 'server', message: `${socket.id} has joined` });
+		socket.emit('message', { name: 'server', message: `Welcome to the Anonymous Chat! You are currently in room "${room}". To join a different room, type /join [room]` });
+	});
+	socket.on('message', (data) => {
+		defaults(socket);
+		let message = data.message.substr(0, 500);
+		let name = socket.id;
+		let sockets = io.of('/').in(socket.proto.room).sockets;
+		let room = socket.proto.room;
+		defaults(sockets, true);
+		message = message.replace(/<.*>/g, '[html]');
+
+		if (message) {
+			if (message[0] === '/') {
+				let newname;
+				switch (message.split(' ')[0]) {
+					case '/key':
+						if (message.split(' ')[1] === process.env.ADMIN) {
+							if (socket.proto.name[0] !== '@') {
+								socket.proto.name = '@' + socket.proto.name;
+								io.to(socket.proto.room).emit('message', { name: 'server', message: `${newname} is now an op` });
+							} else {
+								socket.emit('message', { name: 'server', message: `Error: Already an op` });
+							}
+						} else {
+							socket.emit('message', { name: 'server', message: 'Error: Invalid credentials' });
+						}
+						break;
+					case '/nick':
+						newname = message.split(' ')[1].replace(/\W/g, '');
+						if (!Object.keys(sockets).find(el => sockets[el].proto.name === newname) && newname !== 'server') {
+							socket.proto.name = message.split(' ')[1].replace(/\W/g, '');
+							io.to(socket.proto.room).emit('message', { name: 'server', message: `${name} is now known as ${socket.proto.name}` });
+						} else {
+							socket.emit('message', { name: 'server', message: `Error: Name ${newname} already exists` });
+						}
+						break;
+					case '/help':
+						fs.readFile('cmd.txt', 'utf8', (err, file) => {
+							socket.emit('message', { name: 'server', message: file.replace(/\n/g, ', ') });
+						});
+						break;
+					case '/users':
+						socket.emit('message', { name: 'server', message: Object.keys(sockets).filter(el => sockets[el].proto.room === room).map(el => sockets[el].proto.name).join(', ') });
+						break;
+					case '/rooms':
+						let rooms = occurences(Object.keys(sockets).map(el => sockets[el].proto.room));
+						let users = rooms.b;
+						rooms = rooms.a;
+						// socket.emit('message', { name: 'server', message: Object.keys(sockets).map(el => sockets[el].proto.room).filter((el, i, self) => self.indexOf(el) == i).join(', ') });
+						socket.emit('message', { name: 'server', message: rooms.map((el, i) => `${el} (${users[i]})`).join(', ') });
+						break;
+					default:
+						socket.emit('message', { name: 'server', message: 'Error: Unknown command' });
+				}
+			} else {
+				io.to(socket.proto.room).emit('message', { name: socket.proto.name, message: message, color: socket.id });
+			}
 		}
 	});
-
-	socket.on('disconnect', function () {
-		let ip = (socket.handshake.headers["x-real-ip"] || socket.conn.remoteAddress).replace('::ffff:', '');
-		Object.keys(db.users).find((obj) => {
-			if (db.users[obj].id === socket.id) { // Note SocketID is unique so checking for room isn't needed
-				socket.to(db.users[obj].room).emit('server', `User ${obj} has left the channel`);
-				delete db.users[obj];
-				console.log(`User ${socket.id} (${obj}) has disconnected`);
-				return;
-			}
-		});
-		fs.writeFile('db.json', JSON.stringify(db, null, "\t"), 'utf8', () => {});
+	socket.on('disconnect', (data) => {
+		// socket.to(socket.proto.room).emit('message', { name: 'server', message: `${socket.proto.name} has left` });
 	});
 });
 
-function sanitize(text) {
-	return text.replace(/<(?:.|\n)*>/gi, '[html]');
+function defaults(socket, many) {
+	if (many) {
+		for (let i of Object.keys(socket)) {
+			socket[i].proto = socket[i].proto || {};
+			socket[i].proto.room = socket[i].proto.room || 'main';
+			socket[i].proto.name = socket[i].proto.name || socket[i].id;
+		}
+	} else {
+		socket.proto = socket.proto || {};
+		socket.proto.room = socket.proto.room || 'main';
+		socket.proto.name = socket.proto.name || socket.id;
+	}
 }
 
-function randID() {
-	return Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
+function randHex(len) {
+	var letters = '0123456789abcdef';
+	var color = '';
+	for (var i = 0; i < len; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+function copy(arr) {
+	return JSON.parse(JSON.stringify(arr));
+}
+
+function occurences(arr) {
+	arr = copy(arr);
+	var a = [], b = [], prev;
+	arr.sort();
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] !== prev) {
+			a.push(arr[i]);
+			b.push(1);
+		} else {
+			b[b.length - 1]++;
+		}
+		prev = arr[i];
+	}
+	return {a, b};
 }
---
+++ public/script.js
@@ -0,0 +1,43 @@
+var client = io();
+var room = window.location.pathname;
+var key = false;
+
+$(document).ready(function () {
+	$('.message').focus();
+
+	client.emit('join', room);
+	client.on('message', function (data) {
+		let template = $('#itemTemplate').html();
+		let message = data.message.replace(/http(s)*:\/\/[^\s]*/g, '<a href="$&">$&</a>');
+		let color = data.color || data.name;
+		template = template.replace('{{name}}', data.name);
+		template = template.replace('{{message}}', message);
+		template = template.replace('{{color}}', color);
+		$('.log').append(template);
+		$('.log .item-name').each(function () {
+			$(this).css('color', '#' + $(this).data('color'));
+			if ($(this).text().match('@')) {
+				$(this).css('color', 'red');
+			} else if ($(this).text() === 'server') {
+				if (!$(this).parent().hasClass('server')) {
+					$(this).parent().addClass('server');
+				}
+				$(this).text(' * ');
+			}
+		});
+		$('.log').scrollTop($('.log').height());
+	});
+
+	$('#message').on('keydown', function (e) {
+		let message = $(this).text();
+		if (e.keyCode === 13 && !e.shiftKey) {
+			e.preventDefault();
+			$(this).text('');
+			if (message.indexOf('/join') === 0) {
+				window.location.pathname = '/' + message.split(' ')[1];
+			} else {
+				client.emit('message', { message, key });
+			}
+		}
+	});
+});
