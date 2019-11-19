6ce23458ebf154e0fcf23130c07fb182d8e35b46
vandesm14 <vandesm14@gmail.com>
2019-11-19 16:58:24 -0500

    add cmd syntax errors

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const http = require('http').Server(app);
 const io = require('socket.io').listen(http);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 app.use(express.static('public'));
 
@@ -23,7 +25,7 @@ app.get('*', (req, res) => {
 	res.send('404 Not Found');
 });
 
-http.listen(3000, () => console.log('server started'));
+http.listen(5500, () => console.log('server started'));
 
 io.engine.generateId = (req) => {
 	return randHex(6);
@@ -43,9 +45,16 @@ io.on('connection', (socket) => {
 		socket.proto.room = room;
 		socket.proto.name = socket.id;
 
-		socket.to(room).emit('message', { name: 'server', message: `${socket.id} has joined` });
-		socket.emit('message', { name: 'server', message: `Welcome to the Anonymous Chat! You are currently in room "${room}". To join a different room, type /join [room]` });
+		socket.to(room).emit('message', {
+			name: 'server',
+			message: `${socket.id} has joined`
+		});
+		socket.emit('message', {
+			name: 'server',
+			message: `Welcome to the Anonymous Chat! You are currently in room "${room}". To join a different room, type /join [room]`
+		});
 	});
+
 	socket.on('message', (data) => {
 		defaults(socket);
 		let message = data.message.substr(0, 500);
@@ -54,7 +63,6 @@ io.on('connection', (socket) => {
 		let room = socket.proto.room;
 		defaults(sockets, true);
 		message = message.replace(/<.*>/g, '[html]');
-
 		if (message) {
 			if (message[0] === '/') {
 				let newname;
@@ -63,43 +71,81 @@ io.on('connection', (socket) => {
 						if (message.split(' ')[1] === process.env.ADMIN) {
 							if (socket.proto.name[0] !== '@') {
 								socket.proto.name = '@' + socket.proto.name;
-								io.to(socket.proto.room).emit('message', { name: 'server', message: `${newname} is now an op` });
+								io.to(socket.proto.room).emit('message', {
+									name: 'server',
+									message: `${newname} is now an op`
+								});
 							} else {
-								socket.emit('message', { name: 'server', message: `Error: Already an op` });
+								socket.emit('message', {
+									name: 'server',
+									message: `Error: Already an op`
+								});
 							}
 						} else {
-							socket.emit('message', { name: 'server', message: 'Error: Invalid credentials' });
+							socket.emit('message', {
+								name: 'server',
+								message: 'Error: Invalid credentials'
+							});
 						}
 						break;
 					case '/nick':
-						newname = message.split(' ')[1].replace(/\W/g, '');
-						if (!Object.keys(sockets).find(el => sockets[el].proto.name === newname) && newname !== 'server') {
-							socket.proto.name = message.split(' ')[1].replace(/\W/g, '');
-							io.to(socket.proto.room).emit('message', { name: 'server', message: `${name} is now known as ${socket.proto.name}` });
+						if (message.split(' ')[1] && message.split(' ')[1].replace(/\W/g, '')) {
+							newname = message.split(' ')[1].replace(/\W/g, '');
+							if (!Object.keys(sockets).find(el => sockets[el].proto.name === newname) && newname !== 'server') {
+								socket.proto.name = message.split(' ')[1].replace(/\W/g, '');
+								io.to(socket.proto.room).emit('message', {
+									name: 'server',
+									message: `${name} is now known as ${socket.proto.name}`
+								});
+							} else {
+								socket.emit('message', {
+									name: 'server',
+									message: `Error: Name ${newname} already exists`
+								});
+							}
 						} else {
-							socket.emit('message', { name: 'server', message: `Error: Name ${newname} already exists` });
+							socket.emit('message', {
+								name: 'server',
+								message: `Error: Invalid Syntax`
+							});
 						}
 						break;
 					case '/help':
 						fs.readFile('cmd.txt', 'utf8', (err, file) => {
-							socket.emit('message', { name: 'server', message: file.replace(/\n/g, ', ') });
+							socket.emit('message', {
+								name: 'server',
+								message: file.replace(/\n/g, ', ')
+							});
 						});
 						break;
 					case '/users':
-						socket.emit('message', { name: 'server', message: Object.keys(sockets).filter(el => sockets[el].proto.room === room).map(el => sockets[el].proto.name).join(', ') });
+						socket.emit('message', {
+							name: 'server',
+							message: Object.keys(sockets).filter(el => sockets[el].proto.room === room).map(el => sockets[el].proto.name).join(', ')
+						});
 						break;
 					case '/rooms':
 						let rooms = occurences(Object.keys(sockets).map(el => sockets[el].proto.room));
 						let users = rooms.b;
 						rooms = rooms.a;
 						// socket.emit('message', { name: 'server', message: Object.keys(sockets).map(el => sockets[el].proto.room).filter((el, i, self) => self.indexOf(el) == i).join(', ') });
-						socket.emit('message', { name: 'server', message: rooms.map((el, i) => `${el} (${users[i]})`).join(', ') });
+						socket.emit('message', {
+							name: 'server',
+							message: rooms.map((el, i) => `${el} (${users[i]})`).join(', ')
+						});
 						break;
 					default:
-						socket.emit('message', { name: 'server', message: 'Error: Unknown command' });
+						socket.emit('message', {
+							name: 'server',
+							message: 'Error: Unknown command'
+						});
 				}
 			} else {
-				io.to(socket.proto.room).emit('message', { name: socket.proto.name, message: message, color: socket.id });
+				io.to(socket.proto.room).emit('message', {
+					name: socket.proto.name,
+					message: message,
+					color: socket.id
+				});
 			}
 		}
 	});
@@ -137,7 +183,9 @@ function copy(arr) {
 
 function occurences(arr) {
 	arr = copy(arr);
-	var a = [], b = [], prev;
+	var a = [],
+		b = [],
+		prev;
 	arr.sort();
 	for (var i = 0; i < arr.length; i++) {
 		if (arr[i] !== prev) {
@@ -148,5 +196,8 @@ function occurences(arr) {
 		}
 		prev = arr[i];
 	}
-	return {a, b};
+	return {
+		a,
+		b
+	};
 }
